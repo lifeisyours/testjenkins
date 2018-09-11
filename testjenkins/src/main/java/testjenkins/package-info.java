c738d1dbcd6b377507fe6782b917594682e7acdf
test <domokolo@163.com>
2018-09-11 21:28:57 +0800

    a

---
+++ testjenkins/src/main/java/testjenkins/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Rita
+ * 2018年9月11日
+ * explain:
+ */
+package testjenkins;
---
+++ testjenkins/src/main/java/testjenkins/sayHello.java
@@ -0,0 +1,11 @@
+package testjenkins;
+
+public class sayHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("say hello!");
+
+	}
+
+}
