0c87e21045d983feb5323af15c569df207a577de
test <domokolo@163.com>
2018-09-11 23:07:55 +0800

    121

---
+++ testjenkins/src/test/java/testjenkins/test.java
@@ -0,0 +1,25 @@
+package testjenkins;
+
+import org.testng.annotations.Test;
+
+public class test {
+	@Test
+	private void testjenkins1() {
+		// TODO Auto-generated method stub
+		System.out.println("this is a test1");
+
+	}
+	@Test
+	private void testjenkins2() {
+		// TODO Auto-generated method stub
+		System.out.println("this is a test2");
+
+	}
+	@Test
+	private void testjenkins3() {
+		// TODO Auto-generated method stub
+		System.out.println("this is a test3");
+
+	}
+
+}
