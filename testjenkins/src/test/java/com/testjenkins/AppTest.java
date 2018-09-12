055769d05cd71e9ab51210f3c2c779c85c1eae3d
test <domokolo@163.com>
2018-09-12 13:11:00 +0800

    a

--- testjenkins/src/test/java/com/testjenkins/AppTest.java
+++ testjenkins/src/test/java/com/testjenkins/AppTest.java
@@ -3,6 +3,8 @@ package com.testjenkins;
 
 import static org.testng.Assert.assertTrue;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 
@@ -24,4 +26,18 @@ public class AppTest
     {
         System.out.println("this is a test");
     }
+    @DataProvider( name= "user")
+    public Object[][] user(){
+    return new Object[][]{
+              { "a", "123456"},
+              { "b", "123456"}
+             
+    };
+}
+
+	@Test(dataProvider ="user" )
+	  public void testLogin(String userName,  String passWord) { 
+		      Assert. assertEquals("b",userName);
+	          
+}  
 }
